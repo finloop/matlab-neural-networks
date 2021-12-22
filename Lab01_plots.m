4b4a46819995d6c44250b995aafee44d38c83285
Piotr Krawiec <piotr.krawiec23@gmail.com>
2021-12-22 22:11:15 +0100

    Fix plot ticks

--- Lab01_plots.m
+++ Lab01_plots.m
@@ -1,5 +1,4 @@
 %%%%%%%%%%%%%%%%%%% Wykres wszystkie algorytmy %%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-
 clear
 clc
 training_functions = ["trainscg" "trainrp" "traincgb"];
@@ -31,7 +30,45 @@ xlabel("Ilość neuronów w warstwie ukrytej")
 ylabel("Prezyzja")
 legend(cont, 'Location', 'southwest')
 saveas(gcf, 'img/precision-all.png')
+close(gcf)
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%% Wykresy precyzji per klasa dla algorytmów %%%%%%%%%%%%%%%%%%%
+clear
+clc
+training_functions = ["trainscg" "trainrp" "traincgb"];
+activation_functions = ["tansig" "logsig"];
+
+i = 1;
+cont = {};
+lab = string(17);
+
+for j=[0:16]
+    lab(j+1) = num2str(j*20);
+end
+
+for tfun = training_functions
+    for afun = activation_functions
+        load(tfun +"_"+ afun);
+        plot(ACC, 'LineWidth', 2);
+        xticklabels(lab);
+        title(["Precyzja dla każdej z klas dla kombinacji", ...
+            (tfun +"--"+ afun)])
+        ylim([0.5 1]);
+        for j=[1:12]
+            cont{j} = num2str(j);
+        end
+        
+        leg = legend(cont,'Location', 'southeast');
+        title(leg, "Klasy");
+        leg.Title.Visible = 'on';
+        xlabel("Ilość neuronów w warstwie ukrytej")
+        ylabel("Prezyzja")
+        i = i +1;
+        clear("ACC")
+        saveas(gcf, "img/precision-per-class-" + tfun +"-"+ afun + ".png")
+        close(gcf) 
+    end
+end
+
 
 
