e37604b3dd0476d176783b26d33bc6a8ea983bbf
Piotr Krawiec <piotr.krawiec23@gmail.com>
2022-01-23 13:21:14 +0100

    Update cart and calculate new centers with k-means

--- cart/Lab03.m
+++ cart/Lab03.m
@@ -52,3 +52,21 @@ save("RES.mat")
 % Wykresy:
 % X: ManNumSplits, Y: Acc, dla kryteriów: [gdi, deviance] oran
 % [MinLeafSize(2 lub 4)]
+
+sp = split(RESULTS.Var2, "-");
+criterion = array2table(sp(:, 1));
+criterion.Properties.VariableNames = {'SplitCriterion'};
+min_leaf_size = array2table(str2double(sp(:, 2)));
+min_leaf_size.Properties.VariableNames = {'MinLeafSize'};
+data = [RESULTS(:, [1 3]) criterion min_leaf_size];
+data.Properties.VariableNames = {'MaxNumSplits' 'Acc' 'SplitCriterion' 'MinLeafSize'};
+
+stackedplot(data(data.SplitCriterion == 'gdi', :))
+title('Wpływ parametrów dla SplitCriterion gdi')
+saveas(gcf, 'img/gdi.png')
+close(gcf)
+
+stackedplot(data(data.SplitCriterion == 'deviance', :))
+title('Wpływ parametrów dla SplitCriterion deviance')
+saveas(gcf, 'img/deviance.png')
+close(gcf)
--- k-means/Lab04.m
+++ k-means/Lab04.m
@@ -1,3 +1,4 @@
+clear;
 load("OptDigits5.mat")
 X = OptDigits5(:,1:end-1); 
 Y = OptDigits5(:,end);
@@ -8,13 +9,13 @@ RES = table();
 for class = classes'
     ind_1 = find(Y == class);
     X1 = X(ind_1,:);
-    k1 = round(0.3 * length(ind_1));
+    k1 = round(0.4 * length(ind_1)); % 40 % klas
     [k_1,Klastry_1] = kmeans(X1, k1, 'Distance', 'sqeuclidean');
     RES = [RES; array2table([Klastry_1 i*ones(k1, 1)])]; 
     i = i + 1;
 end
 
-y_new = table2array(RES);
-save('y_new.mat')
+XY = table2array(RES);
+save('XYNEW.mat')
 % Dla nowych danych odpalic najlepszy algorytm
 
